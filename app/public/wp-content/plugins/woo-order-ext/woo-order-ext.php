1c8cfdda8a615d3b2eebfb7630e194bbe4319237
Jörg Adomaitis <joerg.adomaitis@stiftung-enzian.ch>
2026-06-19 10:23:01 +0200

    update plugin

--- app/public/wp-content/plugins/woo-order-ext/woo-order-ext.php
+++ app/public/wp-content/plugins/woo-order-ext/woo-order-ext.php
@@ -20,9 +20,9 @@ defined('ABSPATH') || exit;
 add_action(
 	'init',
 	function () {
-		register_block_type_from_metadata(__DIR__ . '/build/js/checkout-newsletter-subscription-block');
+		register_block_type_from_metadata(__DIR__ . '/src/js/checkout-newsletter-subscription-block');
 		register_block_type_from_metadata(
-			__DIR__ . '/build/js/checkout-greeting-card-block'
+			__DIR__ . '/src/js/checkout-greeting-card-block'
 		);
 	}
 );
