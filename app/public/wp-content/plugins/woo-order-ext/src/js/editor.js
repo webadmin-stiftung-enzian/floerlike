dab460f4b4a7851c5cdff3e5041725a4b0d7c005
Jörg Adomaitis <joerg.adomaitis@stiftung-enzian.ch>
2026-06-19 11:36:33 +0200

    update plugin

--- app/public/wp-content/plugins/woo-order-ext/src/js/editor.js
+++ app/public/wp-content/plugins/woo-order-ext/src/js/editor.js
@@ -1,10 +1,7 @@
 import { registerCheckoutFilters } from '@woocommerce/blocks-checkout';
 
 const modifyAdditionalInnerBlockTypes = ( defaultValue, extensions, args ) => {
-	defaultValue.push( 'woo-order-ext/checkout-newsletter-subscription' );
-	if ( args?.block === 'woocommerce/checkout-additional-information-block' ) {
-		defaultValue.push( 'woo-order-ext/checkout-greeting-card', 'core/gallery' );
-	}
+	defaultValue.push( 'woo-order-ext/checkout-newsletter-subscription', 'woo-order-ext/checkout-greeting-card' );
 	return defaultValue;
 };
 
